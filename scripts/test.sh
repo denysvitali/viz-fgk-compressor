6885d2de77a491983210778ef7458e435ade9f5b
Denys Vitali <denys@denv.it>
2018-01-17 23:30:27 +0100

    Add shellbang to shell script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 make && ./viz -c -f 1.viz test/files/compression/random/1k/01.bin && ./viz -d 1.viz && hexdump -C test/files/compression/random/1k/01.bin | tail && hexdump -C 01.bin | tail && hexdump -C 1.viz | tail
