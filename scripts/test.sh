17b62bc5d4f3be9872781d2fb09e614452e70090
Denys Vitali <denys@denv.it>
2018-01-18 18:34:39 +0100

    Generalize test.sh, add sha256sum check

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
-make && ./viz -c -f 1.viz test/files/compression/random/1k/01.bin && ./viz -d 1.viz && hexdump -C test/files/compression/random/1k/01.bin | tail && hexdump -C 01.bin | tail && hexdump -C 1.viz | tail
+if [ $1 -z ]; then
+  file=1k/01.bin
+else
+  file=$1
+fi
+filename=$(basename $file)
+path=test/files/compression/random
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
+make
+./viz -c -f $filename.viz $path/$file
+./viz -d $filename.viz
+echo -e "\n"
+echo "Original file:"
+hexdump -C $path/$file | tail
+echo -e "\n"
+echo "Extracted file:"
+hexdump -C $filename | tail
+echo -e "\n"
+echo "VIZ file:"
+hexdump -C $filename.viz | tail
+
+sum1=$(sha256sum $path/$file | awk -F ' ' '{print $1}')
+sum2=$(sha256sum $filename | awk -F ' ' '{print $1}')
+
+if [ "$sum1" != "$sum2" ]; then
+  echo -e "${RED}The files are different!$NC"
+  exit 1
+else
+  echo -e "${GREEN}The files are the same.$NC"
+fi
